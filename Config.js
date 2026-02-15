06d68a5327c18cc63c3263af9bb4ab991fdd63e0
6530200851 <sitthipong.kl@ku.th>
2026-02-15 22:03:02 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,13 +1,8 @@
 const CONFIG = {
-  // นำ URL ที่ได้จากตอน Deploy GAS (ตัวที่ลงท้ายด้วย /exec) มาใส่ที่นี่
   GAS_API: "https://script.google.com/macros/s/AKfycbyszNVCvZa7kECf-bk8w5iCdbEEzTb9y0-poJQYF561cnMm7clnX2YZs8mcf_ejlFxOhg/exec",
-  
-  // ใส่ LIFF ID ที่สร้างไว้ใน LINE Developers
   LIFF_ID_ORDER: "2008821220-C8oqqBvB",
   LIFF_ID_STATUS: "2008821220-URtBKHjL",
   LIFF_ID_SLIP: "2008821220-NqDJdKS0",
-  LIFF_ID_DRIVER: "2008821220-M2Bqp8AG"
-  
-  // URL สำหรับส่งสลิป (ถ้ามี)
+  LIFF_ID_DRIVER: "2008821220-M2Bqp8AG", // แก้จุดนี้: เติมคอมม่า
   WEBHOOK_SLIP: "https://amarin-650200908-drinking-water-bot.hf.space/webhook/upload-slip"
 };
