47498e789674ddcf8830b84ca0e0fbc06956ba81
6530200851 <sitthipong.kl@ku.th>
2026-02-16 17:02:36 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -4,6 +4,6 @@ const CONFIG = {
   LIFF_ID_STATUS: "2008821220-URtBKHjL",
   LIFF_ID_SLIP: "2008821220-NqDJdKS0",
   LIFF_ID_DRIVER: "2008821220-M2Bqp8AG",
-  LIFF_ID_Admin: "2008821220-ePnWMlu9",
+  LIFF_ID_ADMIN: "2008821220-ePnWMlu9",
   WEBHOOK_SLIP: "https://amarin-650200908-drinking-water-bot.hf.space/webhook/upload-slip"
 };
