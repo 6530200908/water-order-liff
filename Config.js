93dff7caf0c3f0e3a543be0d0df91937d99fcd99
6530200908 <156393519+6530200908@users.noreply.github.com>
2026-03-02 23:49:54 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  GAS_API: "https://script.google.com/macros/s/AKfycbxOCwDiJWirQRKs76d-kmMicUtClvXsNeYPdXa-S146oD0Xb1k2mgmeLWWGpiq2wO1sfg/exec",
+  GAS_API: "https://script.google.com/macros/s/AKfycbyWJzmLJXyvSJN_Aw8MFhbxGPUT4DhG4gG294NePBd3xxi_90KAHOQLr9sjsB3OoqV8Wg/exec",
   LIFF_ID_ORDER: "2008821220-C8oqqBvB",
   LIFF_ID_STATUS: "2008821220-URtBKHjL",
   LIFF_ID_SLIP: "2008821220-NqDJdKS0",
