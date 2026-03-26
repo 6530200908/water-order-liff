c04fb231e040505385b47d625d7df198adeeac88
6530200851 <sitthipong.kl@ku.th>
2026-03-26 19:42:52 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  GAS_API: "https://script.google.com/macros/s/AKfycbzU80P6UWTNOHJfoczqESPfNmZK8PH_VQxhcjgpoLOLY84UWoU2FbgalUFaP5iZGDOX4A/exec",
+  GAS_API: "https://script.google.com/macros/s/AKfycbzVFky3WNlWwC0GwrpwrXrkqE9uBSO5iYf0jPZKr2-_MGyCZ7_NbVUwMtxCP3D_fnsK1w/exec",
   LIFF_ID_ORDER: "2009604469-stmruoOV",
   LIFF_ID_STATUS: "2009604469-Qu3VzR7E",
   LIFF_ID_SLIP: "2009604469-P2U2iMAw",
